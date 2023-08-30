dbc3d38bf7eca9e93df81c47d2614cbf5fb35a44
CJduncan <136113552+CJduncan@users.noreply.github.com>
2023-08-29 20:28:10 -0600

    files

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './ram.png';
+import './App.css';
+import logo2 from './ram.png';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo2} className="App-logo" alt="logo" />
+        <p>
+          RamHack Loading....
+        </p>
+        
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
